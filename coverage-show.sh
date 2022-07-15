b34e468c0b2e88a2adfc959fd7500c6eecb7be80
David Cuthbert <dacut@kanga.org>
2022-07-15 13:34:45 -0700

    Fix coverage scripts.

--- coverage-show.sh
+++ coverage-show.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export RUSTFLAGS="-Zinstrument-coverage"
+export RUSTFLAGS="-C instrument-coverage"
 export LLVM_PROFILE_FILE="scratchstack-aws-principal-%m.profraw"
 target=target/debug/deps/scratchstack_aws_principal-[a-z0-9][a-z0-9][a-z0-9][a-z0-9][a-z0-9][a-z0-9][a-z0-9][a-z0-9][a-z0-9][a-z0-9][a-z0-9][a-z0-9][a-z0-9][a-z0-9][a-z0-9][a-z0-9]
 echo $target
